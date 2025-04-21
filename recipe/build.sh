ad6f0afde2ea6901bf7f826791a65e3eca6c9a65
David Lecomber <david+dslarm@lecomber.net>
2025-04-21 19:01:05 +0100

    osx arm64 fixes (#13)
    * migration: OSXArm
    * Update meta.yaml: fix linter errs
    * Update meta.yaml: add perl-packagers
    * Update conda-forge.yml: cross-compile due to lack of travis
    * Update meta.yaml: try expat in build (osx-arm64 fails on missing expat)
    * Update meta.yaml: revert expat addn - didn't make a difference
    * Update build.sh: add debug
    * test to see what is happening with compiler in check-devel
    * add patch to fix test for libexpat to not run a target binary if cross compiling
    * MNT: Re-rendered with conda-build 25.4.1, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.19.00.08.06
    
    ---------
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-perl Makefile.PL EXPATLIBPATH=$PREFIX/lib EXPATINCPATH=$PREFIX/include INSTALLDIRS=site
+echo libs..
+ls $PREFIX/lib
+echo include .. 
+ls $PREFIX/include
+
+export EXPATLIBPATH=$PREFIX/lib
+export EXPATINCPATH=$PREFIX/include
+perl Makefile.PL INSTALLDIRS=site
+echo ran makefile.pl
 make
 make install
